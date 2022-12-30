898c9be23035e866892f454d8b61f069fb468df7
Aleksandr <aleksandr.nee@bk.ru>
2022-12-30 19:29:00 +0300

    'memorize-game'

---
+++ js/data/cardsData.js
@@ -0,0 +1,63 @@
+const cards = [
+   {
+      img: "https://images.unsplash.com/photo-1481349518771-20055b2a7b24?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cmFuZG9tfGVufDB8fDB8fA%3D%3D&w=1000&q=80",
+      cardId: "pink-banana",
+   },
+   {
+      img: "https://images.unsplash.com/photo-1494253109108-2e30c049369b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8cmFuZG9tfGVufDB8fDB8fA%3D%3D&w=1000&q=80",
+      cardId: "blue-orange",
+   },
+   {
+      img: "https://images.unsplash.com/photo-1523049673857-eb18f1d7b578?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1075&q=80",
+      cardId: "pink-avocado",
+   },
+   {
+      img: "https://images.unsplash.com/photo-1558364015-0d5ba7a4ba86?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1025&q=80",
+      cardId: "blueberry",
+   },
+   {
+      img: "https://images.unsplash.com/photo-1553909489-cd47e0907980?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1025&q=80",
+      cardId: "sandwich",
+   },
+   {
+      img: "https://images.unsplash.com/photo-1588415986895-48daf87df9b7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+      cardId: "apple",
+   },
+   {
+      img: "https://images.unsplash.com/photo-1558326567-98ae2405596b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=759&q=80",
+      cardId: "macarons",
+   },
+   {
+      img: "https://images.unsplash.com/photo-1540994587294-487f2ffe18f3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1174&q=80",
+      cardId: "red-pepper",
+   },
+   {
+      img: "https://images.unsplash.com/photo-1605672510276-37b74d73eb5a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8cG9zaXRpb258ZW58MHx8MHx8&w=1000&q=80",
+      cardId: "red-banana",
+   },
+   {
+      img: "https://images.unsplash.com/photo-1548199569-3e1c6aa8f469?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=692&q=80",
+      cardId: "pear",
+   },
+   {
+      img : 'https://images.unsplash.com/photo-1601004890684-d8cbf643f5f2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8&w=1000&q=80',
+      cardId : 'strawberry'
+   },
+   {
+      img : 'https://media.istockphoto.com/id/1344251913/photo/pitaya-or-dragon-fruit.jpg?b=1&s=170667a&w=0&k=20&c=j5BjmD6VPifpABR4BykJxgTgNtEMztM3PtJpUvxxvTM=',
+      cardId : 'dragon-fruit'
+   },
+   {
+      img : 'https://media.istockphoto.com/id/1293137857/photo/ripe-whole-kiwi-fruit-and-half-kiwi-fruit-isolated-on-white-background-full-depth-of-field.jpg?b=1&s=170667a&w=0&k=20&c=PeZKrgYiYUyjaiTEieTvLfDKHKzfbKJ57O7T3fuBq0Q=',
+      cardId : 'qiwi'
+   },
+   {
+      img : 'https://img.theculturetrip.com/1024x/wp-content/uploads/2016/11/oscar-ivan-esquivel-arteaga-guanabana-unsplash.jpg',
+      cardId : 'durian'
+   },
+   {
+      img : 'https://i0.wp.com/post.healthline.com/wp-content/uploads/2021/11/watermelon-1296x728-header-1.jpg?w=1155&h=1528',
+      cardId : 'watermelon'
+   }
+];
+export default cards
---
+++ js/main.js
@@ -0,0 +1,92 @@
+import cardsData from "./data/cardsData.js";
+const selectBlock = document.querySelector(".select-level");
+const mainContainer = document.querySelector(".main__container");
+selectBlock.addEventListener("click", (e) => {
+   switch (e.target.textContent.trim()) {
+      case "Easy":
+         sortCardsData(cardsData.slice(0, 5));
+         break;
+      case "Medium":
+         sortCardsData(cardsData.slice(0, 10));
+         break;
+      case "Hard":
+         sortCardsData(cardsData);
+   }
+});
+const sortCardsData = (data) => {
+   data = [...data, ...data];
+   data.sort(() => Math.random() - 0.5);
+   generateData(data);
+};
+const createCard = (src, cardsContainer, config) => {
+   const card = document.createElement("div");
+   const cardImg = document.createElement("img");
+   const cardBack = document.createElement("div");
+   card.appendChild(cardImg);
+   card.appendChild(cardBack);
+   card.className = config.card;
+   cardImg.className = config.img;
+   cardBack.className = config.back;
+   cardBack.textContent = '?'
+   card.dataset.id = config.id;
+   cardImg.src = src;
+   cardsContainer.appendChild(card);
+   flipCard(card);
+};
+const flipCard = (card) => {
+   setTimeout(() => {
+      card.classList.add("card--flip");
+      mainContainer.style.pointerEvents = "none";
+      setTimeout(() => {
+         card.classList.remove("card--flip");
+         mainContainer.style.pointerEvents = "all";
+         card.addEventListener("click", (e) => {
+            card.classList.toggle("card--flip");
+            checkAnswer(e.target.parentElement);
+         });
+      }, 5000);
+   }, 1000);
+};
+const generateData = (cardData) => {
+   mainContainer.innerHTML = ''
+   const cardsContainer = document.createElement("div");
+   cardsContainer.className = "cards-container";
+   cardData.forEach((cardObj) => {
+      createCard(cardObj.img, cardsContainer, {
+         card: "card",
+         img: "card__img",
+         back: "card__back",
+         id: cardObj.cardId,
+      });
+   });
+   mainContainer.append(cardsContainer);
+};
+const checkAnswer = (target) => {
+   target.classList.add("flipped");
+   const flippedCards = document.querySelectorAll(".flipped");
+   if (flippedCards.length === 2) {
+      const [firstCard, secondCard] = flippedCards;
+      if (firstCard.dataset.id === secondCard.dataset.id) {
+         flippedCards.forEach((card) => {
+            setTimeout(() => {
+               card.innerHTML = "";
+               card.classList.remove("flipped", "card--flip");
+               const allCards = document.querySelectorAll('.card')
+               restartGame(allCards)
+            }, 700);
+         });
+      } else {
+         flippedCards.forEach((card) => {
+            card.classList.remove("flipped");
+            setTimeout(() => {
+               card.classList.remove("card--flip");
+            }, 700);
+         });
+      }
+   }
+};
+const restartGame = (allCards) => {
+    if(Array.from(allCards).every(card => card.innerHTML === '')){
+        window.location.reload()
+    }
+}
